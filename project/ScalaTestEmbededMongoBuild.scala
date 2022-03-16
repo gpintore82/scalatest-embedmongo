ac348ef2a54fcdc7ad172e7555c6406ccebc3a0a
gpintore82 <57242719+gpintore82@users.noreply.github.com>
2022-03-16 12:05:43 -0700

    Scala 2.13

--- project/ScalaTestEmbededMongoBuild.scala
+++ project/ScalaTestEmbededMongoBuild.scala
@@ -16,9 +16,9 @@ object ScalaTestEmbededMongoBuild extends Build {
       crossScalaVersions := Seq("2.11.12", "2.12.8", "2.13.4"),
 
       libraryDependencies ++= Seq(
-        "de.flapdoodle.embed" % "de.flapdoodle.embed.mongo" % "2.0.0",
-        "org.scalatest" %% "scalatest" % "3.0.1" % "test",
-        "org.mongodb.scala" %% "mongo-scala-driver" % "1.2.1" % "test"
+        "de.flapdoodle.embed" % "de.flapdoodle.embed.mongo" % "3.4.4",
+        "org.scalatest" %% "scalatest" % "3.2.2" % "test",
+        "org.mongodb.scala" %% "mongo-scala-driver" % "4.2.0" % "test"
       ),
 
       parallelExecution := false,
